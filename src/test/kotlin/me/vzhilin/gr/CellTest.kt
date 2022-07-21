f16dfc301bd6b0c8ecbe4abf9ab4c71ca4e6af3d
vzhn <vzhilin.me@gmail.com>
2022-07-21 19:54:54 +0200

    cells

--- src/test/kotlin/me/vzhilin/gr/CellTest.kt
+++ src/test/kotlin/me/vzhilin/gr/CellTest.kt
@@ -3,7 +3,7 @@ package me.vzhilin.gr
 import kotlin.test.Test
 
 class CellTest {
-    private val rules = listOf(
+    val rulesList = listOf(
         Term("SPACE", " "),
         Term("DOT", "."),
         Term("X", "x"),
@@ -17,14 +17,38 @@ class CellTest {
         Prod("V", "NAMES"),
         Prod("APP", "T", "SPACE", "T"),
         Prod("ABST", "LAMBDA", "V", "DOT", "T")
-    ).groupByTo(mutableMapOf()) { it.name }
+    )
+    private val rulesMap = rulesList.associateBy { it.name }
 
     @Test
     fun test() {
+        fun ix(n: String) = rulesList.indexOf(rulesMap[n]!!)
         val input = "λx.y λx.y"
-
+        val row0 = listOf(
+            /*  'λ'   */ Cell(0, ix("LAMBDA"), 0),
+            /*  'x'   */ Cell(1, ix("X"), 0),
+            /*  '.'   */ Cell(2, ix("DOT"), 0),
+            /*  'y'   */ Cell(3, ix("Y"), 0),
+            /*  ' '   */ Cell(4, ix("SPACE"), 0),
+            /*  'λ'   */ Cell(5, ix("LAMBDA"), 0),
+            /*  'x'   */ Cell(6, ix("X"), 0),
+            /*  '.'   */ Cell(7, ix("DOT"), 0),
+            /*  'y'   */ Cell(8, ix("Y"), 0),
+        )
+        println(row0)
     }
 }
+
+data class Cell(
+    // row number
+    val i: Int,
+
+    // rule number
+    val rn: Int,
+
+    // position in rule
+    val rp: Int
+)
 
 /**
 
