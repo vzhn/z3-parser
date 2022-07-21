d4f2a4df989a160ad219001c4ab792f3f66acd32
vzhn <vzhilin.me@gmail.com>
2022-07-21 17:27:51 +0200

    init

---
+++ src/test/kotlin/me/vzhilin/gr/CellTest.kt
@@ -0,0 +1,36 @@
+package me.vzhilin.gr
+
+import kotlin.test.Test
+
+class CellTest {
+    private val rules = listOf(
+        Term("SPACE", " "),
+        Term("DOT", "."),
+        Term("X", "x"),
+        Term("Y", "y"),
+        Term("Z", "z"),
+        Term("LAMBDA", "λ"),
+        Term("DOT", "."),
+        Sum("NAMES", "X", "Y", "Z"),
+
+        Sum("T", "V", "APP", "ABST"),
+        Prod("V", "NAMES"),
+        Prod("APP", "T", "SPACE", "T"),
+        Prod("ABST", "LAMBDA", "V", "DOT", "T")
+    ).groupByTo(mutableMapOf()) { it.name }
+
+    @Test
+    fun test() {
+        val input = "λx.y λx.y"
+
+    }
+}
+
+/**
+
+ CELL
+ 1. Rule, for example ABST ⟶ 'λ' V '.' T
+ 2. Part of rule, for example V
+ 3. position in the part of rule
+ 4. terminal
+ */
---
+++ src/test/kotlin/me/vzhilin/gr/Rules.kt
@@ -0,0 +1,30 @@
+package me.vzhilin.gr
+
+sealed class Rule {
+    abstract val name: String
+}
+
+data class Sum(override val name: String, val args: List<Ref>): Rule() {
+    constructor(name: String, vararg refs: String): this(name, refs.map(::Ref))
+
+    override fun toString(): String {
+        return "$name → ${args.joinToString(" ")}"
+    }
+}
+data class Prod(override val name: String, val args: List<Ref>): Rule() {
+    constructor(name: String, vararg refs: String): this(name, refs.map(::Ref))
+
+    override fun toString(): String {
+        return "$name → ${args.joinToString(" | ")}"
+    }
+}
+data class Term(override val name: String, val value: String): Rule() {
+    override fun toString(): String {
+        return "$name → '$value'"
+    }
+}
+data class Ref(override val name: String): Rule() {
+    override fun toString(): String {
+        return name
+    }
+}
