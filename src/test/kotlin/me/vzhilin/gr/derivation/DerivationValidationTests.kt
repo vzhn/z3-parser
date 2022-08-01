1d7ce1a7e79827a03ccdced41bf06d6669b45272
vzhn <vzhilin.me@gmail.com>
2022-08-01 19:54:01 +0200

    refactoring grammar

--- src/test/kotlin/me/vzhilin/gr/derivation/DerivationValidationTests.kt
+++ src/test/kotlin/me/vzhilin/gr/derivation/DerivationValidationTests.kt
@@ -18,22 +18,22 @@ import kotlin.test.assertEquals
 
 class DerivationValidationTests {
     private val input =
-"""     λ x . x λ y . y y                #  V(3)                       # λ x . V(x) λ y . y y
-        λ x . V(x) λ y . y y             #  T(3)                       # λ x . T(x) λ y . y y
-        λ x . T(x) λ y . y y             #  V(1)                       # λ V(x) . T(x) λ y . y y
-        λ V(x) . T(x) λ y . y y          #  ABST(0:3)                  # ABST(λx.x)    λ y . y y
-        ABST(λx.x)    λ y . y y          #  V(2)                       # ABST(λx.x)    λ V(y) . y y
-        ABST(λx.x)    λ V(y) . y y       #  V(4)                       # ABST(λx.x)    λ V(y) . V(y) y
-        ABST(λx.x)    λ V(y) . V(y) y    #  T(4)                       # ABST(λx.x)    λ V(y) . T(y) y
-        ABST(λx.x)    λ V(y) . T(y) y    #  ABST(1:4)                  # ABST(λx.x)    ABST(λy.y)    y
-        ABST(λx.x)    ABST(λy.y)    y    #  T(0)                       # T(λx.x)       ABST(λy.y)    y
-        T(λx.x)       ABST(λy.y)    y    #  T(1)                       # T(λx.x)       T(λy.y)       y
-        T(λx.x)       T(λy.y)       y    #  APP(0:1)                   # APP(λx.xλy.y)               y
-        APP(λx.xλy.y)               y    #  T(0)                       # T(λx.xλy.y)                 y
-        T(λx.xλy.y)                 y    #  V(1)                       # T(λx.xλy.y)                 V(y)
-        T(λx.xλy.y)                 V(y) #  T(1)                       # T(λx.xλy.y)                 T(y)
-        T(λx.xλy.y)                 T(y) #  APP(0:1)                   # APP(λx.xλy.yy)                  
-        APP(λx.xλy.yy)                   #  T(0)                       # T(λx.xλy.yy)
+    """ λ x . x λ y . y y            #  V(3)         # λ x . V(x) λ y . y y
+        λ x . V(x) λ y . y y         #  T(3)         # λ x . T(x) λ y . y y
+        λ x . T(x) λ y . y y         #  V(1)         # λ V(x) . T(x) λ y . y y
+        λ V(x) . T(x) λ y . y y      #  ABST(0:3)    # ABST(λx.x) λ y . y y
+        ABST(λx.x) λ y . y y         #  V(2)         # ABST(λx.x) λ V(y) . y y
+        ABST(λx.x) λ V(y) . y y      #  V(4)         # ABST(λx.x) λ V(y) . V(y) y
+        ABST(λx.x) λ V(y) . V(y) y   #  T(4)         # ABST(λx.x) λ V(y) . T(y) y
+        ABST(λx.x) λ V(y) . T(y) y   #  ABST(1:4)    # ABST(λx.x) ABST(λy.y) y
+        ABST(λx.x) ABST(λy.y) y      #  T(0)         # T(λx.x) ABST(λy.y) y
+        T(λx.x) ABST(λy.y) y         #  T(1)         # T(λx.x) T(λy.y) y
+        T(λx.x) T(λy.y) y            #  APP(0:1)     # APP(λx.xλy.y) y
+        APP(λx.xλy.y) y              #  T(0)         # T(λx.xλy.y) y
+        T(λx.xλy.y) y                #  V(1)         # T(λx.xλy.y) V(y)
+        T(λx.xλy.y) V(y)             #  T(1)         # T(λx.xλy.y) T(y)
+        T(λx.xλy.y) T(y)             #  APP(0:1)     # APP(λx.xλy.yy)                  
+        APP(λx.xλy.yy)               #  T(0)         # T(λx.xλy.yy)
     """.trimIndent()
 
     @Test
